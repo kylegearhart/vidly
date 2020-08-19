47ef782db7c85c0641f042e2a4b019610d7e9a3b
Kyle Gearhart <hkgearhart@gmail.com>
2020-08-19 16:13:31 -0500

    Allow toggling of logs using DEBUG env var

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const logger = require('debug')('app:startup')
+
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
@@ -5,6 +7,6 @@ const port = process.env.PORT || 3000
 app.listen(
   3000,
   () => {
-    console.log(`Vidley launched and listening on port ${port}.`)
+    logger(`Vidley launched and listening on port ${port}.`)
   },
 )
