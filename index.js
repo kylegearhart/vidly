11ad93175640b021626634e516f240e63fb50e5c
Kyle Gearhart <hkgearhart@gmail.com>
2020-08-19 17:29:59 -0500

    Connect to MongoDB on boot; separate http and db logs

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const logger = require('debug')('app:startup')
+const httpLogger = require('debug')('app:http')
+const dbLogger = require('debug')('app:db')
 const express = require('express')
+const mongoose = require('mongoose')
 const genres = require('./genres')
 const port = process.env.PORT || 3000
 
@@ -7,9 +9,14 @@ const app = express()
 app.use(express.json())
 app.use('/api/genres', genres)
 
+const mongoDBOptions = { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.connect('mongodb://localhost/vidly', mongoDBOptions)
+  .then(() => dbLogger('Successfully connected to MongoDB database called vidly on localhost'))
+  .catch((error) => dbLogger('Failed to connect to vidly MongoDB database on localhost', error))
+
 app.listen(
   port,
   () => {
-    logger(`Vidly launched and listening on port ${port}.`)
+    httpLogger(`Vidly launched and listening on port ${port}.`)
   },
 )
