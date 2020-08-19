aac106834ed904d97b6d5a5de25d925f915fe9fe
Kyle Gearhart <hkgearhart@gmail.com>
2020-08-19 16:35:34 -0500

    Create new genre by POST /api/genres (name must be 2 chars or more long)

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const logger = require('debug')('app:startup')
-
 const express = require('express')
+const Joi = require('joi')
+
 const app = express()
+app.use(express.json())
 const port = process.env.PORT || 3000
 
 app.listen(
@@ -16,6 +18,20 @@ const genres = [
   { id: 2, name: 'Comedy' },
   { id: 3, name: 'Drama' },
 ]
+
+app.post('/api/genres', (request, response) => {
+  const genreSchema = Joi.object({
+    name: Joi.string().min(2).required()
+  })
+  const validationResult = genreSchema.validate(request.body)
+
+  if (validationResult.error) return response.status(400).send(validationResult.error.message)
+
+  const newGenre = { id: genres.length + 1, name: request.body.name }
+  genres.push(newGenre)
+
+  return response.send(newGenre)
+})
 
 app.get('/api/genres', (request, response) => {
   response.send(genres)
