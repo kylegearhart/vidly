212c4231485eb2ef2151d8af4ded63618f4a2e1f
Kyle Gearhart <hkgearhart@gmail.com>
2020-08-19 17:16:11 -0500

    Re-organize code lines for readability

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const logger = require('debug')('app:startup')
 const express = require('express')
 const genres = require('./genres')
+const port = process.env.PORT || 3000
 
 const app = express()
 app.use(express.json())
 app.use('/api/genres', genres)
-const port = process.env.PORT || 3000
 
 app.listen(
   port,
