ac51538df292dbe38784201832b1512bfa74f88b
Kyle Gearhart <hkgearhart@gmail.com>
2020-08-19 16:07:47 -0500

    Set default port to 3000, and utilize env var PORT if set

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000
 
 app.listen(
   3000,
   () => {
-    console.log('Vidley launched and listening on port 3000.')
+    console.log(`Vidley launched and listening on port ${port}.`)
   },
 )
