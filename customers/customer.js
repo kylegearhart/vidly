cc02730f4154f71e325ed69e051262be0fef5e42
Kyle Gearhart <hkgearhart@gmail.com>
2020-08-22 12:54:24 -0500

    Standardize all String min/max validation to 5-50 chars

--- customers/customer.js
+++ customers/customer.js
@@ -3,15 +3,15 @@ const Joi = require('joi')
 
 module.exports.Customer = mongoose.model('Customer', {
   isGold: { type: Boolean, required: true },
-  name: { type: String, minlength: 8, maxlength: 30, required: true },
-  phone: { type: String, minlength: 7, maxlength: 15, required: true },
+  name: { type: String, minlength: 5, maxlength: 50, required: true },
+  phone: { type: String, minlength: 5, maxlength: 50, required: true },
 })
 
 module.exports.validateAsCustomer = (customer) => {
   const validationSchema = Joi.object({
     isGold: Joi.boolean().required(),
-    name: Joi.string().min(8).max(30).required(),
-    phone: Joi.string().min(7).max(15).required(),
+    name: Joi.string().min(5).max(50).required(),
+    phone: Joi.string().min(5).max(50).required(),
   })
 
   return validationSchema.validate(customer)
--- genres/genre.js
+++ genres/genre.js
@@ -4,13 +4,13 @@ const Joi = require('joi')
 module.exports.Genre = mongoose.model(
   'Genre',
   mongoose.Schema({
-    name: { type: String, minlength: 2, maxlength: 30, required: true },
+    name: { type: String, minlength: 5, maxlength: 50, required: true },
   }),
 )
 
 module.exports.validateAsGenre = (genre) => {
   const validationSchema = Joi.object({
-    name: Joi.string().min(2).required(),
+    name: Joi.string().min(5).max(50).required(),
   })
 
   return validationSchema.validate(genre)
