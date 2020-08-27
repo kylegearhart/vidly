4334bd511d11065731fa15b6fb96f5451f01539e
Kyle Gearhart <hkgearhart@gmail.com>
2020-08-27 16:43:45 -0500

    Give higher character limit for email and password

--- users/user.js
+++ users/user.js
@@ -3,15 +3,15 @@ const Joi = require('joi')
 
 User = mongoose.model('User', {
   name: { type: String, minlength: 5, maxlength: 50, required: true },
-  email: { type: String, minlength: 5, maxlength: 50, required: true, unique: true },
-  password: { type: String, minlength: 5, maxlength: 50, required: true },
+  email: { type: String, minlength: 5, maxlength: 255, required: true, unique: true },
+  password: { type: String, minlength: 5, maxlength: 1024, required: true },
 })
 
 validateAsUser = (customer) => {
   const validationSchema = Joi.object({
     name: Joi.string().min(5).max(50).required(),
-    email: Joi.string().min(5).max(50).required(),
-    password: Joi.string().min(5).max(50).required(),
+    email: Joi.string().min(5).max(255).required(),
+    password: Joi.string().min(5).max(1024).required(),
   })
 
   return validationSchema.validate(customer)
