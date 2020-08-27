450fa8d80ab517718481c5413ad44b4b67a6274e
Kyle Gearhart <hkgearhart@gmail.com>
2020-08-27 17:04:23 -0500

    Use _.pick to strip unwanted data from request body and response

--- users/userController.js
+++ users/userController.js
@@ -3,6 +3,7 @@ const { User, validateAsUser } = require('./user')
 const repository = require('./userRepository')
 const express = require('express')
 const router = express.Router()
+const _ = require('lodash')
 
 router.post('/', async (request, response) => {
   let validationResult = validateAsUser(request.body)
@@ -12,10 +13,10 @@ router.post('/', async (request, response) => {
     const duplicatedUser = await User.findOne({ email: request.body.email })
     if (duplicatedUser) return response.status(400).send('User with email already exists.')
 
-    const savedOrUpdatedUser = await repository.add(request.body)
+    const savedOrUpdatedUser = await repository.add(_.pick(request.body, ['name', 'email', 'password']))
     if (!savedOrUpdatedUser) return response.status(500).send('User save was unsuccessful.')
 
-    return response.send(savedOrUpdatedUser)
+    return response.send(_.pick(savedOrUpdatedUser, ['name', 'email']))
   } catch (error) {
     errorLogger(error.message)
     return response.status(500).send('User creation was unsuccessful.')
